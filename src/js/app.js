06d47d4f0e00e9a6754b28d54bee21efe957920d
Samuel ademola <samuel.ademola43@gmail.com>
2020-07-19 10:55:28 +0100

    added material fonts

--- src/js/app.js
+++ src/js/app.js
@@ -35,3 +35,13 @@ var app = new Vue({
   mounted() {
   }
 });
+
+ if ('serviceWorker' in navigator) {
+   window.addEventListener('load', () => {
+     navigator.serviceWorker.register('/service-worker.js').then(registration => {
+       console.log('SW registered: ', registration);
+     }).catch(registrationError => {
+       console.log('SW registration failed: ', registrationError);
+     });
+   });
+ }
